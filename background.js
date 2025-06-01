314ff891a431d2a95ec1518ffdec2edd9f127ce0
813hoyeon <pphy08133@hufs.ac.kr>
2025-06-01 23:27:00 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const images = ['cutecat.jpg', 'cutecat2.jpg', 'cutecat3.jpg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundImage = `url('${chosenImage}')`;
 document.body.style.backgroundSize = 'cover';
 document.body.style.backgroundPosition = 'center';
 document.body.style.backgroundRepeat = 'no-repeat';
