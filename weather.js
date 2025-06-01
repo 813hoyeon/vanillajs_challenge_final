ce3b0e4d2270a8113f455510918f8b403f03058a
813hoyeon <pphy08133@hufs.ac.kr>
2025-06-01 23:36:36 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -17,7 +17,19 @@ function onGeoOk(position) {
     });
 }
 function onGeoError() {
-  alert("Can't find you. No weather for you.");
+  const lat = 37.4979;
+  const lon = 127.0276;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
+
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
